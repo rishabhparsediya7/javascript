e6d9501c225ab3b5ffb285c4e15b9874c4e8379c
rishabhparsediya7 <parsediyarishabh@gmail.com>
2025-01-12 19:17:45 +0530

    debounce function

---
+++ debounce/index.js
@@ -0,0 +1,19 @@
+const myDebounce = (cb, d) => {
+  let timer;
+  return function (...args) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      cb(...args);
+    }, d);
+  };
+};
+
+const incrementCount = myDebounce(() => {
+  console.log("incremented");
+}, 1000);
+
+const click = () => {
+  incrementCount();
+};
+
+click();
